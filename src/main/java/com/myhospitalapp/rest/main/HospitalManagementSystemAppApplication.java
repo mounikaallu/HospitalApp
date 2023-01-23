9e63930b1e6b1f1568d64f218b6236a790bad57d
Mounika Allu <86432516+mounikaallu@users.noreply.github.com>
2023-01-23 14:58:35 +0530

    first commit

---
+++ src/main/java/com/myhospitalapp/rest/main/HospitalManagementSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.myhospitalapp.rest.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemAppApplication.class, args);
+	}
+
+}
